ebc348bcc678b9e198e165765044ecdf290f6d31
Mathias Kaerlev <matpow2@gmail.com>
2015-10-10 02:44:51 +0200

    Missing Android files

---
+++ Chowdren/base/android/sdlfile.cpp
@@ -0,0 +1,67 @@
+#include <SDL_rwops.h>
+
+void BaseFile::open(const char * filename, const char * mode)
+{
+    flags = 0;
+    const char * new_mode;
+    switch (*mode) {
+        case 'r':
+            new_mode = "rb";
+            break;
+        case 'w':
+            flags |= WRITE;
+            new_mode = "wb";
+            break;
+    }
+
+    std::string file_string = convert_path(filename);
+    const char * file_string_c = file_string.c_str();
+    SDL_RWops * new_handle = SDL_RWFromFile(file_string_c, new_mode);
+    if (new_handle == NULL) {
+        flags |= CLOSED;
+        return;
+    }
+    handle = (void*)new_handle;
+}
+
+bool BaseFile::seek(size_t v, int origin)
+{
+    SDL_RWops * rw = (SDL_RWops*)handle;
+    return SDL_RWseek(rw, v, origin) >= 0;
+}
+
+size_t BaseFile::tell()
+{
+    SDL_RWops * rw = (SDL_RWops*)handle;
+    return SDL_RWtell(rw);
+}
+
+size_t BaseFile::read(void * data, size_t size)
+{
+    SDL_RWops * rw = (SDL_RWops*)handle;
+    return SDL_RWread(rw, data, 1, size);
+}
+
+size_t BaseFile::write(const void * data, size_t size)
+{
+    SDL_RWops * rw = (SDL_RWops*)handle;
+    return SDL_RWwrite(rw, data, 1, size);
+}
+
+bool BaseFile::at_end()
+{
+    SDL_RWops * rw = (SDL_RWops*)handle;
+    Sint64 pos = SDL_RWtell(rw);
+    if (SDL_RWseek(rw, 0, RW_SEEK_END) == pos)
+        return true;
+    SDL_RWseek(rw, pos, RW_SEEK_SET);
+}
+
+void BaseFile::close()
+{
+    if (flags & CLOSED)
+        return;
+    SDL_RWops * rw = (SDL_RWops*)handle;
+    SDL_RWclose(rw);
+    flags |= CLOSED;
+}
